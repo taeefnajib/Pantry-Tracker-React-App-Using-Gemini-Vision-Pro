6523eb9a8f6c834ba3c4909484aaaae146b3ecd6
taeefnajib <taeefnajib@gmail.com>
2024-08-04 00:31:51 +0600

    remove test image and products from process

--- Backend/process.py
+++ Backend/process.py
@@ -8,8 +8,8 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-img = PIL.Image.open('./image/image4.jpg')
+# img = PIL.Image.open('./image/image4.jpg')
 
-products = "football, desklamp, book, jeans pants"
+# products = "football, desklamp, book, jeans pants"
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
